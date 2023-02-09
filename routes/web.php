72f46e06f5c1dc2815a69dc56ce43b9fccd73c5c
Akil Ahmed <akilahmed.b15@gmail.com>
2023-02-09 22:18:39 +0600

    route model binding

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,9 @@ Route::get('/', function () {
 });
 
 // Single Listing
-Route::get('listings/{id}', function ($id) {
-    $listing = Listing::find($id);
-
-    if ($listing) {
-        return view('listing', [
-            'listing' => $listing
-        ]);
-    } else {
-        abort('404');
-    }
+Route::get('listings/{listing}', function (Listing $listing) {
+    
+    return view('listing', [
+        'listing' => $listing
+    ]);
 });
