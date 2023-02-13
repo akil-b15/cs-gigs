f692ba4fb0b651d4ecfde75855362c7fe56ab2f5
Akil Ahmed <akilahmed.b15@gmail.com>
2023-02-13 20:59:43 +0600

    controller shift

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+
+
+class ListingController extends Controller
+{
+    // all listing 
+    public function index() {
+        return view('listings.index', [
+            'listings' => Listing::all()
+        ]);
+    }
+
+    // single listing 
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
@@ -13,18 +14,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
+// Common Resource Routes:
+// 1. index - show all listings
+// 2. show - show a single listing
+// 3. create - show form for creating new listing
+// 4. store - store new listing
+// 5. destroy - delete a listing
+// 6. update - update a listing
+// 7. edit - show form for editing
+
+
+
 // All Listing
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single Listing
-Route::get('listings/{listing}', function (Listing $listing) {
-    
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('listings/{listing}', [ListingController::class, 'show']);
